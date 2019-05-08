4e82c2012745b1e17284785736ff6bd89f4045e4
AprilGrimoire <AprilGrimoire@gmail.com>
2019-05-08 18:35:19 +0800

    Added route system

--- resources/views/static_pages/index.blade.php
+++ resources/views/static_pages/index.blade.php
@@ -3,4 +3,5 @@
 @section('content')
     <a href="{{ route('home') }}">用户</a><br />
     <a href="{{ route('problems.index') }}">题目列表</a><br />
+    <a href="{{ route('submissions.index') }}">提交记录</a><br />
 @stop
