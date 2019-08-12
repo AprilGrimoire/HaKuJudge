258b8f15cbc5d7d041afbfe1be5c30e3656063ba
AprilGrimoire <AprilGrimoire@gmail.com>
2019-08-12 17:30:36 +0800

    Centered the paginate bar in question pool

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
         @yield('content')
--- resources/views/problems/index.blade.php
+++ resources/views/problems/index.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts/default')
 
 @section('content')
-    @foreach ($problems as $problem)
-        <a href="{{ route('problems.show', $problem) }}">{{ $problem->title }}</a><br />
-    @endforeach
-    {!! $problems->render() !!}
+    <div class="container">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>编号</th>
+                    <th>题目名称</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($problems as $problem)
+                    <tr>
+                        <th>{{ $problem->id }}</th>
+                        <th><a href="{{ route('problems.show', $problem) }}">{{ $problem->title }}</a></th>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div class="text-center">
+            {!! $problems->render() !!}
+        </div>
+    </div>
 @stop
